75cc98cce742e8ea91959923f71feea28e496cbc
xorazmiy1996 <muhammadibragimov439@gmail.com>
2023-05-05 11:54:18 +0500

    static rut update

--- config/settings.py
+++ config/settings.py
@@ -125,10 +125,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My config
-STATICFILES_DIRS = [
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/'),
+# ]
+STATIC_ROOT =[
     os.path.join(BASE_DIR, 'static/'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 from django.utils.translation import gettext_lazy as _
 
